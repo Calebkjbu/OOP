88e378f7dd76d65d128d29c32239cc5c2a67b20c
Calebkjbu <KempkaC@jbu.edu>
2025-08-27 14:07:27 -0500

    Add files via upload

---
+++ cw-1.py
@@ -0,0 +1,7 @@
+nameEmply = input("What is your Name?")
+wage = int(input("how mcuh do you make an hour"))
+hours = int(input("how many hours per day"))
+days = int(input("how many days per week do you work"))
+total = wage * hours * days * 52
+print("your yearly salary is", total,"$")
+
