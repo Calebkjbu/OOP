e61b2d4fb8e61e7592fe5f3e0e253a9ee0ab036d
KempkaC <KempkaC@jbu.edu>
2025-09-24 14:24:18 -0500

    yup

---
+++ cw-9.py
@@ -0,0 +1,31 @@
+list = []
+x =1
+def enqueue():
+    addvar = input("What would you like to add to the queue")
+    list.append(addvar)
+def dequeue():
+    rem = list[0]
+    list.remove(rem)
+def display():
+    print(list)
+
+
+while x == 1:
+    print("1.Enqueue")
+    print("2.Dequeue")
+    print("3.Display")
+    print("4.Quit")
+
+    option = int(input("What is the option you choosin? : "))
+
+    if option == 1:
+        enqueue()
+    elif option == 2:
+        dequeue()
+    elif option == 3:
+        display()
+    elif option == 4:
+        break
+    else:
+        print("BOOHOO BUD")
+        break
