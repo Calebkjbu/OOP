777d1b17f4494ff60df0911de53f357589faf997
Calebkjbu <KempkaC@jbu.edu>
2025-09-24 09:45:47 -0500

    Add files via upload

---
+++ cw-9.py
@@ -0,0 +1,30 @@
+list = []
+x =1
+def enqueue():
+    addvar = input("What would you like to add to the queue")
+    list.append(addvar)
+def dequeue():
+    rem = list[0]
+    list.remove(rem)
+def display():
+    print(list)
+
+
+while x == 1:
+    print("1.Enqueue")
+    print("2.Dequeue")
+    print("3.Display")
+    print("4.Quit")
+
+    option = int(input("What is the option you chooin"))
+
+    if option == 1:
+        enqueue()
+    elif option == 2:
+        dequeue()
+    elif option == 3:
+        display()
+    elif option ==4:
+        break
+    else:
+        break
