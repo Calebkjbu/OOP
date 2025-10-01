cc7a114a144e7ac1b5517f199ff8895971c5a9c6
KempkaC <KempkaC@jbu.edu>
2025-10-01 09:26:14 -0500

    main

--- cw-10.py
+++ cw-10.py
@@ -1,1 +1,20 @@
-diu
+class Student:
+    def __init__(self):
+        self.stu.id = ""
+        self.stu_Name = ""
+        self.major = ""
+        self.gpa = 0.0
+        self.dop = ""
+    def add_student(self):
+        self.stu.id = input("Enter Stu ID")
+        self.stu_Name = input("Enter Student Name")
+        self.major = input("Enter Student's Major")
+        self.gpa = input("Enter Student's Gpa")
+        self.dop = input("Enter Date of Birth")
+    def edit_student(self):
+
+    def display_student(self):
+
+
+
+s1 = Student()
